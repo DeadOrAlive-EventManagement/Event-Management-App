ca32348c9742803015df25236ab9f19726a527d7
JyothsnaKS <jyo.somanna@gmail.com>
2018-10-10 14:54:43 +0530

    modified the database since there were errors in previous version and added few sample insert statements

--- src/database/create_tables.sql
+++ src/database/create_tables.sql
@@ -1,130 +1,63 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS Bookings;
+DROP TABLE IF EXISTS Customer;
+DROP TABLE IF EXISTS Events;
+DROP TABLE IF EXISTS Services;
+DROP TABLE IF EXISTS Vendor;
+SET FOREIGN_KEY_CHECKS = 1;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+DROP DATABASE SE_Project;
 
-CREATE DATABASE IF NOT EXISTS `SE_Project` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `SE_Project`;
+CREATE DATABASE SE_Project;
+USE SE_Project;
 
-CREATE TABLE `Bookings` (
-  `book_id` int(11) NOT NULL,
-  `vendor_id` int(11) NOT NULL,
-  `customer_id` int(11) DEFAULT NULL,
-  `service_id` int(11) DEFAULT NULL,
-  `booking_status` varchar(50) DEFAULT NULL
+CREATE TABLE Customer (
+  customer_id int(11) PRIMARY KEY AUTO_INCREMENT,
+  email varchar(100) UNIQUE,
+  first_name varchar(20) NOT NULL,
+  middle_name varchar(20) DEFAULT NULL,
+  last_name varchar(20) NOT NULL,
+  phone_number char(10) DEFAULT 0000000000,
+  pwd varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `Bookings` (`book_id`, `vendor_id`, `customer_id`, `service_id`, `booking_status`) VALUES
-(60, 20, 2, 90, 'Pending'),
-(61, 21, 1, 91, 'Accepted');
-
-CREATE TABLE `Customer` (
-  `customer_id` int(11) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `first_name` varchar(20) NOT NULL,
-  `middle_name` varchar(20) DEFAULT NULL,
-  `last_name` varchar(20) NOT NULL,
-  `phone_number` char(10) DEFAULT '0000000000',
-  `pwd` varchar(100) NOT NULL
+CREATE TABLE Events (
+  event_id int(11) PRIMARY KEY AUTO_INCREMENT,
+  event_name varchar(100) NOT NULL,
+  customer_id int(11),
+  budget int(11) NOT NULL,
+  num_people int(11) DEFAULT NULL,
+  date_event date DEFAULT NULL,
+  FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `Customer` (`customer_id`, `email`, `first_name`, `middle_name`, `last_name`, `phone_number`, `pwd`) VALUES
-(1, 'acb@gmail.com', 'acb', 'ed', 'efg', '9658741230', 'acbedefg'),
-(2, 'xyz@gmail.com', 'xyz', NULL, 'uvw', '6758942536', 'uvwxyz');
-
-CREATE TABLE `Events` (
-  `event_id` int(11) NOT NULL,
-  `event_name` varchar(100) NOT NULL,
-  `customer_id` int(11) DEFAULT NULL,
-  `budget` int(11) NOT NULL,
-  `num_people` int(11) DEFAULT NULL,
-  `date_event` date DEFAULT NULL,
-  `service_id` int(11) DEFAULT NULL,
-  `vendor_id` int(11) DEFAULT NULL
+CREATE TABLE Vendor (
+  vendor_id int(11) PRIMARY KEY AUTO_INCREMENT,
+  vendor_name varchar(100) DEFAULT NULL,
+  vendor_location varchar(150) DEFAULT NULL,
+  email varchar(100) UNIQUE,
+  phone_number char(10) DEFAULT 0000000000,
+  pwd varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `Events` (`event_id`, `event_name`, `customer_id`, `budget`, `num_people`, `date_event`, `service_id`, `vendor_id`) VALUES
-(300, 'birthday', 1, 10000, 50, '2018-10-31', 90, 20);
-
-CREATE TABLE `Services` (
-  `service_id` int(11) NOT NULL,
-  `vendor_id` int(11) NOT NULL,
-  `service_name` varchar(100) NOT NULL,
-  `price_per_unit` decimal(10,0) NOT NULL,
-  `service_type` varchar(100) DEFAULT NULL
+CREATE TABLE Services (
+  service_id int(11),
+  vendor_id int(11),
+  service_name varchar(100) NOT NULL,
+  price_per_unit decimal(10,0) NOT NULL,
+  service_type varchar(100) DEFAULT NULL,
+  FOREIGN KEY (vendor_id) REFERENCES Vendor(vendor_id),
+  PRIMARY KEY(vendor_id, service_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `Services` (`service_id`, `vendor_id`, `service_name`, `price_per_unit`, `service_type`) VALUES
-(90, 20, 'lunch food', '30', 'food'),
-(91, 21, 'lights', '50', 'lights');
-
-CREATE TABLE `Vendor` (
-  `vendor_id` int(11) NOT NULL,
-  `vendor_name` varchar(100) DEFAULT NULL,
-  `vendor_location` varchar(150) DEFAULT NULL
+CREATE TABLE Bookings (
+  event_id int(11),
+  vendor_id int(11),
+  customer_id int(11),
+  service_id int(11),
+  booking_status varchar(50) DEFAULT FALSE,
+  FOREIGN KEY (event_id) REFERENCES Events(event_id),
+  FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
+  FOREIGN KEY (vendor_id, service_id) REFERENCES Services(vendor_id, service_id),
+  PRIMARY KEY (event_id, customer_id, vendor_id, service_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO `Vendor` (`vendor_id`, `vendor_name`, `vendor_location`) VALUES
-(20, 'creepers', 'blore'),
-(21, 'sleepers', 'MUMBAI');
-
-
-ALTER TABLE `Bookings`
-  ADD PRIMARY KEY (`book_id`),
-  ADD KEY `vendor_id` (`vendor_id`),
-  ADD KEY `customer_id` (`customer_id`),
-  ADD KEY `service_id` (`service_id`);
-
-ALTER TABLE `Customer`
-  ADD PRIMARY KEY (`customer_id`),
-  ADD UNIQUE KEY `email` (`email`);
-
-ALTER TABLE `Events`
-  ADD PRIMARY KEY (`event_id`),
-  ADD KEY `customer_id` (`customer_id`),
-  ADD KEY `service_id` (`service_id`),
-  ADD KEY `vendor_id` (`vendor_id`);
-
-ALTER TABLE `Services`
-  ADD PRIMARY KEY (`service_id`,`vendor_id`),
-  ADD KEY `vendor_id` (`vendor_id`);
-
-ALTER TABLE `Vendor`
-  ADD PRIMARY KEY (`vendor_id`);
-
-
-ALTER TABLE `Bookings`
-  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
-
-ALTER TABLE `Customer`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
-ALTER TABLE `Events`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=301;
-
-ALTER TABLE `Vendor`
-  MODIFY `vendor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
-
-
-ALTER TABLE `Bookings`
-  ADD CONSTRAINT `Bookings_ibfk_1` FOREIGN KEY (`vendor_id`) REFERENCES `Vendor` (`vendor_id`),
-  ADD CONSTRAINT `Bookings_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `Customer` (`customer_id`),
-  ADD CONSTRAINT `Bookings_ibfk_3` FOREIGN KEY (`service_id`) REFERENCES `Services` (`service_id`);
-
-ALTER TABLE `Events`
-  ADD CONSTRAINT `Events_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `Customer` (`customer_id`),
-  ADD CONSTRAINT `Events_ibfk_2` FOREIGN KEY (`service_id`) REFERENCES `Services` (`service_id`),
-  ADD CONSTRAINT `Events_ibfk_3` FOREIGN KEY (`vendor_id`) REFERENCES `Vendor` (`vendor_id`);
-
-ALTER TABLE `Services`
-  ADD CONSTRAINT `Services_ibfk_1` FOREIGN KEY (`vendor_id`) REFERENCES `Vendor` (`vendor_id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/database/dummy_insert.sql
+++ src/database/dummy_insert.sql
@@ -9,21 +9,24 @@ TRUNCATE Bookings;
 SET FOREIGN_KEY_CHECKS = 1;
 
 INSERT INTO Customer ( email, first_name, middle_name, last_name, phone_number, pwd) VALUES
-('acb@gmail.com', 'acb', 'ed', 'efg', '9658741230', 'acbedefg'),
-('xyz@gmail.com', 'xyz', NULL, 'uvw', '6758942536', 'uvwxyz');
+('mike@gmail.com', 'Mike', NULL, 'Jordan', '9658741230', 'mike'),
+('ashley@gmail.com', 'Ashley', NULL, 'Park', '6758942536', 'ashley');
 
-INSERT INTO Vendor ( vendor_name, vendor_location) VALUES
-('creepers', 'blore'),
-('sleepers', 'MUMBAI');
+INSERT INTO Vendor (vendor_name, vendor_location) VALUES
+('HKG Caterers', 'Bangalore'),
+('Ivy Park Venue', 'Bangalore');
 
-INSERT INTO Services (vendor_id, service_name, price_per_unit, service_type) VALUES
-(1, 'lunch food', '30', 'food'),
-(2, 'lights', '50', 'lights');
+INSERT INTO Services (vendor_id, service_id, service_name, price_per_unit, service_type) VALUES
+(1, 1, 'Lunch Food', '30', 'Catering'),
+(2, 1, 'Venue for organising small functions', '50', 'Venue');
 
-INSERT INTO Events (event_name, customer_id, budget, num_people, date_event, service_id) VALUES
-('birthday', 1, 10000, 50, '2018-10-31', 1);
+INSERT INTO Events (event_name, customer_id, budget, num_people, date_event) VALUES
+('Birthday', 1, 10000, 20, '2018-10-31'),
+('Custom Event', 1, 50000, 50, '2018-11-06');
 
-INSERT INTO Bookings (vendor_id, customer_id, service_id, booking_status) VALUES
-(1, 2, 1, 'Pending'),
-(2, 1, 2, 'Accepted');
+INSERT INTO Bookings (event_id, vendor_id, customer_id, service_id, booking_status) VALUES
+(1, 1, 1, 1, 'Pending'),
+(1, 2, 1, 1, 'Accepted'),
+(2, 1, 1, 1, 'Pending'),
+(2, 2, 1, 1, 'Accepted');
 
