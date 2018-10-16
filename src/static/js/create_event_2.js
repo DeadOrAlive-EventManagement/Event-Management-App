c83785d3bf11db5d521dd9b77a3e9e8d485da380
Jyotsna Nayak <jns0897@gmail.com>
2018-10-16 11:41:58 +0530

    enhanced version of create_event

---
+++ src/static/js/create_event_2.js
@@ -0,0 +1,51 @@
+function populate_venue()
+{
+    row_val ="row";
+    col_value = "col s3";
+    var div_row = document.createElement("div");
+    div_row.setAttribute("class" ,"collapsible-body");
+    var p_main = document.createElement("p");
+    p_main.innerHTML ="Choose from our reputed vendors";
+    div_row.appendChild(p_main);
+    
+    //   <div class="row">
+    //     <div class="col s3">
+    //       <div class="card blue darken-1">
+    //         <div class="card-content white-text">
+    //           <span class="card-title">Ezra Home Delivery</span>
+    //           <p class= "card-text">I am a very simple card. I am good at containing small bits of information.
+    //           I am convenient because I require little markup to use effectively.</p>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div class="col s3">
+    //       <div class="card blue darken-1" >
+    //        <div class="card-content white-text">
+    //           <span class="card-title">Tom's Bakery</span>
+    //           <p class= "card-text">I am a very simple card. I am good at containing small bits of information.
+    //           I am convenient because I require little markup to use effectively.</p>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div class="col s3">
+    //       <div class="card blue darken-1">
+    //         <div class="card-content white-text">
+    //           <span class="card-title">Timmy's Peaches</span>
+    //           <p>I am a very simple card. I am good at containing small bits of information.
+    //           I am convenient because I require little markup to use effectively.</p>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div class="col s3">
+    //       <div class="card blue darken-1">
+    //         <div class="card-content white-text">
+    //           <span class="card-title">Noah's Cafe</span>
+    //           <p>I am a very simple card. I am good at containing small bits of information.
+    //           I am convenient because I require little markup to use effectively.</p>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    //
+    // </div>
+}
